69f436ba60c8fc096a3258000998403ed59d1c3f
Maksim Skrypka <skrypka.m.l@gmail.com>
2022-07-02 14:40:02 +0300

    Initial Commit

---
+++ NatifeTaskUITests/NatifeTaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NatifeTaskUITestsLaunchTests.swift
+//  NatifeTaskUITests
+//
+//  Created by Maksim Skrypka on 02.07.2022.
+//
+
+import XCTest
+
+class NatifeTaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
